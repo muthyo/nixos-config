4a6655505979c47faf39e03e9bcba44f0296c225
muthyo <git@skau.simplelogin.com>
2026-03-27 17:15:18 +0100

    fix(home): clean up redundant vim configuration in editors.nix
    Remove the xdg.configFile symlink that incorrectly placed .vimrc in
    ~/.config/ and the commented-out home.file line. programs.vim with
    extraConfig is the proper home-manager approach and handles it already.

--- home/muthyo/editors.nix
+++ home/muthyo/editors.nix
@@ -6,12 +6,8 @@
   # Configure Vim properly
   programs.vim = {
     enable = true;
-
-    # This is the important part - it pulls content from your vimrc file
     extraConfig = builtins.readFile ../dotfiles/vimrc;
   };
-  # home.file.".vimrc".source = ../dotfiles/vimrc;
-  xdg.configFile.".vimrc".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/home/dotfiles/vimrc";
 
   # Configure Helix with symlinks
   programs.helix.enable = true;
